b17c5daed29d42a409d0b73c7d6da2694f09de35
Priya Acharjee <priyaacharjee2001@gmail.com>
2025-10-05 00:45:54 +0530

    Data Association

--- AuthenticationandAuthorization/app.js
+++ AuthenticationandAuthorization/app.js
@@ -1,23 +1,23 @@
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const app = express();
-const bcrypt=require('bcrypt');
-const jwt = require('jsonwebtoken');
+// const cookieParser = require('cookie-parser');
+// const express = require('express');
+// const app = express();
+// const bcrypt=require('bcrypt');
+// const jwt = require('jsonwebtoken');
 
 
-app.use(cookieParser());
+// app.use(cookieParser());
 
-app.get("/",function(req,res){
-    let token = jwt.sign({email:"priya@gmail.com"},"secret");
-    res.cookie("token",token);
-    res.send("done");
+// app.get("/",function(req,res){
+//     let token = jwt.sign({email:"priya@gmail.com"},"secret");
+//     res.cookie("token",token);
+//     res.send("done");
     
-})
+// })
 
-app.get("/read",function(req,res){
-    let data = jwt.verify(req.cookies.token,"secret");
-    console.log(data);    
-})
+// app.get("/read",function(req,res){
+//     let data = jwt.verify(req.cookies.token,"secret");
+//     console.log(data);    
+// })
 
 //app.get("/read",function(req,res){
     // bcrypt.compare("Priya@123","$2b$10$j37IpqsCzpZaZJbWKk/az.0nhw0j/JHAsQPJfmDW7Ll8Fwm2u1Pxq",function(err,result){
@@ -35,4 +35,39 @@ app.get("/read",function(req,res){
 //     })
 // })
 
+//app.listen(3000);
+
+const express = require('express');
+const app = express();
+const userModel = require("./models/user");
+const postModel = require("./models/post");
+
+app.get("/",(req,res)=>{
+    res.send("hey");
+})
+
+app.get("/create", async (req,res)=>{
+    let user= await userModel.create({
+        username:"Priya",
+        age:23,
+        email:"priya@gmail.com"
+    });
+    res.send(user);
+})
+
+app.get("/post/create", async (req,res)=>{
+    let post = await postModel.create({
+        postdata:"hello everyone",
+        user:"68e15be286c3623194694afa"
+    })
+
+    let user = await userModel.findOne({_id:"68e15be286c3623194694afa"});
+    user.posts.push(post._id);
+    await user.save();
+
+    res.send({post,user});
+})
+
 app.listen(3000);
+
+
--- AuthenticationandAuthorization/models/user.js
+++ AuthenticationandAuthorization/models/user.js
@@ -3,10 +3,16 @@ const mongoose = require('mongoose');
 mongoose.connect(`mongodb://127.0.0.1:27017/authtestapp`);
 
 const userSchema = mongoose.Schema({
-    username:String,
-    email:String,
-    password:String,
-    age:Number
+    username: String,
+    email: String,
+    password: String,
+    age: Number,
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'post'
+        }
+    ]
 });
 
-module.exports = mongoose.model("user",userSchema);
+module.exports = mongoose.model("user", userSchema);
