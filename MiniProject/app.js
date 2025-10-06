f67e591e991a79468feb05182e80698a5c54d952
Priya Acharjee <priyaacharjee2001@gmail.com>
2025-10-07 02:07:14 +0530

    Mini Project part 2

--- MiniProject/app.js
+++ MiniProject/app.js
@@ -20,9 +20,9 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.get("/profile", isLoggedIn, (req, res) => {
-    console.log(req.user);
-    res.render("login");
+app.get("/profile", isLoggedIn,async (req, res) => {
+    let user = await userModel.findOne({email:req.user.email}).populate("posts");
+    res.render("profile",{user});
 });
 
 app.post("/register", async (req, res) => {
@@ -55,7 +55,7 @@ app.post("/login", async (req, res) => {
         if (result) {
             let token = jwt.sign({ email: email, userid: user._id }, "secret");
             res.cookie("token", token);
-            res.status(200).send("you can login");
+            res.status(200).redirect("/profile");
         }
         else res.redirect("/login");
     })
@@ -66,8 +66,22 @@ app.get('/logout', (req, res) => {
     res.redirect("/login");
 });
 
+app.post("/post", isLoggedIn,async (req, res) => {
+    let user = await userModel.findOne({email:req.user.email});
+    let {content}=req.body;
+
+    let post = await postModel.create({
+        user:user._id,
+        content
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect("/profile");
+});
+
 function isLoggedIn(req, res, next) {
-    if (req.cookies.token === "") res.send("You must be logged in");
+    if (req.cookies.token === "") res.redirect("/login");
     else {
         let data = jwt.verify(req.cookies.token, "secret");
         req.user = data;
